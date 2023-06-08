8f3f8cc7626f5b7bfbcf3e9f3435c6393bf33aae
GyanDennis <gyangh23@gmail.com>
2023-06-08 09:41:58 -0700

    3x is cool

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ == "__main__":
+    from sys import argv
+    sum = 0
+    for i, _ in enumerate(argv):
+        if i != 0:
+            sum += int(_)
+    print(sum)

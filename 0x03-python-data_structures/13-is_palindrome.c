7e25a1b6552ce433f68b5e2bfac43c05d2609b07
GyanDennis <gyangh23@gmail.com>
2023-06-10 01:31:09 -0700

    palindrome done

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,79 +1,62 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+int list_len(listint_t *);
+
 /**
-* is_palindrome - Checks if a singly linked list is a palindrome
-* @head: The head of the singly linked list
-*
-* Return: 0 if it is not a palindrome, 1 if it is a palindrome
-*/
+ * is_palindrome - check if a linked list is a palindrome
+ * @head: address of pointer to the head of the list
+ *
+ * Return: 0 if not a palindrome, 1 if a palindrome
+ */
 int is_palindrome(listint_t **head)
 {
-listint_t *start = NULL, *end = NULL;
-unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+	int list_length = list_len(*head);
+	int array[4096];
+	int half_length = list_length / 2;
+	int i;
+	listint_t *current_node = NULL;
 
-if (head == NULL)
-return (0);
-if (*head == NULL)
-return (1);
+	/* Check if head pointer is NULL */
+	if (!head)
+		exit(-1);
 
-start = *head;
-len = listint_len(start);
-len_cyc = len * 2;
-len_list = len_cyc - 2;
-end = *head;
+	/* If the list has more than one element */
+	if (list_length > 1)
+	{
+		/* Store the first half of the list in an array */
+		for (i = 0, current_node = *head;
+		     i < half_length;
+		     i++, current_node = current_node->next)
+		{
+			array[i] = current_node->n;
+		}
 
-for (; i < len_cyc; i = i + 2)
-{
-if (start[i].n != end[len_list].n)
-return (0);
+		/* If the list has odd length, move to the next node */
+		if (list_length % 2)
+			current_node = current_node->next;
 
-len_list = len_list - 2;
-}
+		/* Compare the second half of the list with the reversed 1st h*/
+		for (i = 0; i < half_length; i++, current_node = current_node->next)
+		{
+			if (array[half_length - i - 1] != current_node->n)
+				return (0); /* Not a palindrome */
+		}
+	}
 
-return (1);
-}
-
-/**
-* get_nodeint_at_index - Gets a node from a linked list
-* @head: The head of the linked list
-* @index: The index to find in the linked list
-* Return: The specific node of the linked list
-*/
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-listint_t *current = head;
-unsigned int iter_times = 0;
-
-if (head)
-{
-while (current != NULL)
-{
-if (iter_times == index)
-return (current);
-
-current = current->next;
-++iter_times;
-}
-}
-
-return (NULL);
+	return (1); /* Palindrome */
 }
 
 /**
-* slistint_len - Counts the number of elements in a linked list
-* @h: The linked list to count
-* Return: Number of elements in the linked list
-*/
-size_t listint_len(const listint_t *h)
+ * list_len - count the number of nodes in a linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: number of nodes
+ */
+int list_len(listint_t *head)
 {
-int lenght = 0;
-
-while (h != NULL)
-{
-++lenght;
-h = h->next;
-}
-
-return (lenght);
+	if (head)
+		return (1 + list_len(head->next));
+	return (0);
 }

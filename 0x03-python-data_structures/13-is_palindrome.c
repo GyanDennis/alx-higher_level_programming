ef565661ddfa26bcaeeb5740b691fc7c404172aa
GyanDennis <gyangh23@gmail.comgit config --global user.email gyangh23@gmail.com>
2023-06-28 12:59:49 -0700

    done

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,62 +1,62 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 
-int list_len(listint_t *);
+/* Definition for singly-linked list. */
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
 
-/**
- * is_palindrome - check if a linked list is a palindrome
- * @head: address of pointer to the head of the list
- *
- * Return: 0 if not a palindrome, 1 if a palindrome
- */
-int is_palindrome(listint_t **head)
-{
-	int list_length = list_len(*head);
-	int array[4096];
-	int half_length = list_length / 2;
-	int i;
-	listint_t *current_node = NULL;
+/* Function to reverse a linked list. */
+struct ListNode *reverseList(struct ListNode *head) {
+    struct ListNode *prev = NULL;
+    struct ListNode *curr = head;
+    struct ListNode *next = NULL;
 
-	/* Check if head pointer is NULL */
-	if (!head)
-		exit(-1);
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
 
-	/* If the list has more than one element */
-	if (list_length > 1)
-	{
-		/* Store the first half of the list in an array */
-		for (i = 0, current_node = *head;
-		     i < half_length;
-		     i++, current_node = current_node->next)
-		{
-			array[i] = current_node->n;
-		}
-
-		/* If the list has odd length, move to the next node */
-		if (list_length % 2)
-			current_node = current_node->next;
-
-		/* Compare the second half of the list with the reversed 1st h*/
-		for (i = 0; i < half_length; i++, current_node = current_node->next)
-		{
-			if (array[half_length - i - 1] != current_node->n)
-				return (0); /* Not a palindrome */
-		}
-	}
-
-	return (1); /* Palindrome */
+    return prev;
 }
 
-/**
- * list_len - count the number of nodes in a linked list
- * @head: pointer to the head of the list
- *
- * Return: number of nodes
- */
-int list_len(listint_t *head)
-{
-	if (head)
-		return (1 + list_len(head->next));
-	return (0);
+/* Function to check if a singly linked list is a palindrome. */
+bool isPalindrome(struct ListNode* head) {
+    /* Count the number of nodes in the linked list. */
+    int count = 0;
+    struct ListNode *current = head;
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+
+    /* If the list has 0 or 1 nodes, it is a palindrome. */
+    if (count == 0 || count == 1) {
+        return true;
+    }
+
+    /* Find the middle node of the linked list. */
+    int mid = count / 2;
+    current = head;
+    for (int i = 0; i < mid; i++) {
+        current = current->next;
+    }
+
+    /* Reverse the second half of the linked list. */
+    struct ListNode *secondHalf = reverseList(current);
+
+    /* Compare the first half of the linked list with the reversed second half. */
+    struct ListNode *firstHalf = head;
+    for (int i = 0; i < mid; i++) {
+        if (firstHalf->val != secondHalf->val) {
+            return false;
+        }
+        firstHalf = firstHalf->next;
+        secondHalf = secondHalf->next;
+    }
+
+    return true;
 }

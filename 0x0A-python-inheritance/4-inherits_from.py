e2e75152aad81308b23957dc2c8b03697058b4ae
GyanDennis <gyangh23@gmail.com>
2023-07-10 16:16:30 +0000

    python cracked

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""
-Contains the inherits_from function
-"""
+
+"""The module is 4-inherits_from"""
+
+
 def inherits_from(obj, a_class):
-"""returns true if obj is a subclass of a_class, otherwise false"""
-return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """
+    a function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class

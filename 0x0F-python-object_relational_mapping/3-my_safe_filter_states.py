62d0e71832f90092ef11befe9ca7abadfa99a460
GyanDennis <gyangh23@gmail.com>
2023-09-17 10:38:46 +0000

    object mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,42 +1,20 @@
 #!/usr/bin/python3
 """
-Safe script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
+Display all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+
+It has to be safe from MySQL injections!
 """
-
+import sys
 import MySQLdb
-from sys import argv
 
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-def main():
-    # Extract command line arguments
-    username, password, database, search_name = argv[1], argv[2], argv[3], argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    states = cur.fetchall()
 
-    # Connect to the MySQL server
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    # Create a cursor to execute queries
-    cur = conn.cursor()
-
-    # Create the SQL query with user input using parameterized query
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (search_name,))
-
-    # Fetch and display the results
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-
-    # Close the cursor and connection
-    cur.close()
-    conn.close()
-
-
-if __name__ == "__main__":
-    main()
+    for state in states:
+        print(state)
